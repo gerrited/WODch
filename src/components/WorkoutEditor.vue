80fd0bc0e93c074ca7cd36e8848107b1e733d76a
Gerrit Edzards <gerrit.edzards@gmail.com>
2026-06-25 23:19:27 +0200

    feat: add WorkoutEditor component

---
+++ src/components/WorkoutEditor.vue
@@ -0,0 +1,25 @@
+<template>
+  <textarea
+    class="workout-editor"
+    placeholder="WOD eingeben..."
+    spellcheck="false"
+  />
+</template>
+
+<style scoped>
+.workout-editor {
+  width: 100%;
+  height: 100%;
+  background: #000;
+  color: #fff;
+  font-family: 'JetBrains Mono', monospace;
+  font-size: 16px;
+  line-height: 1.7;
+  border: none;
+  outline: none;
+  resize: none;
+  padding: 20px;
+}
+
+.workout-editor::placeholder { color: #333; }
+</style>
